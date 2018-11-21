987e054d4f161a9dbffaf20e9be50f03c05c2f1c
phungltk <whatsmahname8@gmail.com>
2018-11-21 08:13:35 +0700

    enter

---
+++ QTweb/QTweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QTweb.Startup))]
+namespace QTweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
